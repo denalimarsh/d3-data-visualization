5ec051d4f5e83596bb95e425eb3f7adddb3ce3fe
denalimarsh <denalimarsh@gmail.com>
2018-01-17 23:37:17 -0800

    here you go

---
+++ java-api-hierarchy-visualized/d3/treemap.js
@@ -0,0 +1,165 @@
+
+
+// used to color node by depth
+var color = d3.scaleOrdinal();
+
+// accessor functions for x and y
+var x = function(d) { return d.x; };
+var y = function(d) { return d.y; };
+
+// normal line generator
+var line = d3.line()
+  .curve(d3.curveLinear)
+  .x(x)
+  .y(y);
+
+// configure size, margin, and circle radius
+var config = {
+    w: 900,
+    h: 450,
+    r: 4,
+    pad: 10
+};
+
+// maximum diameter of circle is minimum dimension
+config.d = Math.min(config.w, config.h);
+
+var file = "https://gist.githubusercontent.com/sjengle/bae96276eb80c0f1b61e58cef47b2529/raw/6cf67e78f17a96d9332d7c001568781b0fb9cf1e/java8.csv";
+
+
+d3.csv(file, convert, callback);
+  
+function convert(row) {
+  var parts = row.name.split(".");
+  row.parent = parts[parts.length - 1];
+  row.size = +row.size;
+  return row;
+}
+  
+  
+function callback(error, data) {
+    if (error) {
+        console.warn(file, error);
+        return;
+    }
+  
+    console.log("data:", data.length, data);
+
+    // used to create hierarchies
+    // https://github.com/d3/d3-hierarchy#stratify
+    var stratify = d3.stratify()
+      .id(function(d) { return d.name; })
+      .parentId(function(d) {
+        // should match existing id (except for root)
+        return d.name.substring(0, d.name.lastIndexOf("."));
+      });
+  
+    // convert csv into hierarchy
+    var root = stratify(data);
+  
+    // sort by height then value
+    // https://github.com/d3/d3-hierarchy#node_sort
+    root.sort(function(a, b) {
+        if (a.height != b.height) {
+          return d3.ascending(a.height, b.height);
+        }
+        else {
+          return d3.ascending(a.size, b.size);
+        }
+      });
+  
+    console.log("root:", root);
+  
+    // setup color scale
+    color.domain(d3.range(root.height + 1));
+    color.range(d3.schemeOrRd[root.height + 1]);
+
+    drawTreeMap("drawMap", root.copy());
+    
+}
+
+function drawNodes(g, nodes, raise) {
+  g.selectAll("circle")
+    .data(nodes)
+    .enter()
+    .append("circle")
+      .attr("r", function(d) { return d.r ? d.r : config.r; })
+      .attr("cx", x)
+      .attr("cy", y)
+      .attr("id", function(d) { return d.data.name; })
+      .attr("class", "node")
+      .style("fill", function(d) {return color(d.depth)})
+      .on("mouseover.tooltip", function(d) {
+        show_tooltip(g, d3.select(this));
+        d3.select(this).classed("selected", true);
+        if (raise) {
+          d3.select(this).raise();
+        }
+      })
+      .on("mouseout.tooltip", function(d) {
+        g.select("#tooltip").remove();
+        d3.select(this).classed("selected", false);
+      });
+}
+
+function drawLinks(g, links, generator) {
+  var paths = g.selectAll("path")
+    .data(links)
+    .enter()
+    .append("path")
+    .attr("d", generator)
+    .attr("class", "link");
+}
+
+function drawTreeMap(id, root) {
+  var svg = d3.select("body").select("#" + id);
+  svg.attr("width", 950);
+  svg.attr("height", 500);
+
+  var g = svg.append("g");
+  g.attr("id", "plot");
+  g.attr("transform", translate(75, 80));
+
+  // calculate sum for nested circles
+  root.sum(function(d) { return d.size; });
+
+  // setup treemap layout
+  var treemap = d3.treemap()
+    .padding(8)
+    .size([ 800,
+            400]);
+
+  // run layout to calculate x, y, and r attributes
+  treemap(root);
+
+  // draw nested rectangles
+  var rects = g.selectAll("rect")
+    .data(root.descendants())
+    .enter()
+    .append("rect")
+    .attr("x", function(d) { return d.x0; })
+    .attr("y", function(d) { return d.y0; })
+    .attr("width", function(d) { return d.x1 - d.x0; })
+    .attr("height", function(d) { return d.y1 - d.y0; })
+    .attr("id", function(d) { return d.data.name; })
+    .attr("class", "node")
+    .style("fill", function(d) {return color(d.depth)})
+    .on("mouseover.tooltip", function(d) {
+      show_tooltip(g, d3.select(this));
+      d3.select(this).classed("selected", true);
+    })
+    .on("mouseout.tooltip", function(d) {
+      g.select("#tooltip").remove();
+      d3.select(this).classed("selected", false);
+    });
+
+     //Title text
+  svg.append("text")
+    .attr("x", (950 / 2))             
+    .attr("y", 45) 
+    .attr("text-anchor", "middle")  
+      .attr("fill", "black")
+    .style("font-size", "28px")
+    .text("Treemap Proportional Area");
+
+}
---
+++ san-francisco-tree-maintenance/d3/script.js
@@ -0,0 +1,208 @@
+var token = "oHGHafj30eFYC608qFovVphfl";
+
+// the geojson files are large, so loading them locally
+var urls = {
+  basemap: "NeighborhoodMap.geojson",
+  streets: "streets.geojson",
+  trees: "https://data.sfgov.org/resource/d9d2-cuwu.json"
+};
+
+// add parameters to trees url
+urls.trees += "?$$app_token=" + token;
+
+var margin = {top: 50, left: 0};
+
+var svg = d3.select("body").select("svg")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var g = {
+  basemap: svg.append("g").attr("id", "basemap"),
+  trees: svg.append("g").attr("id", "trees"),
+  tooltip: svg.append("g").attr("id", "tooltip"),
+  details: svg.append("g").attr("id", "details")
+};
+ 
+// https://github.com/d3/d3-geo#conic-projections
+var projection = d3.geoConicEqualArea();
+var path = d3.geoPath().projection(projection);
+
+// http://mynasadata.larc.nasa.gov/latitudelongitude-finder/
+// center on san francisco [longitude, latitude]
+// choose parallels on either side of center
+projection.parallels([37.692514, 37.840699]);
+
+// rotate to view we usually see of sf
+projection.rotate([122, 0]);
+
+var color = d3.scaleOrdinal()
+  .domain(["Closed", "Open"])
+  .range(['red','green']);
+
+  var legend = svg.selectAll(".legend")
+     .data(["Closed", "Open"])
+.enter().append("g")
+     .attr("class", "legend")
+     .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+// draw legend colored rectangles
+legend.append("circle")
+     .attr("class", "dot")
+     .attr("r", 8)
+     .attr("cx", 100)
+     .attr("cy", 320)
+     .style("fill", function(d){return color(d)});
+
+// draw legend text
+legend.append("text")
+     .attr("x", 85)
+     .attr("y", 320)
+     .attr("dy", ".35em")
+     .style("font-size", "14")
+     .style("text-anchor", "end")
+     .text(function(d) { return d;});
+
+ //Title text
+svg.append("text")
+  .attr("x", (1440 / 2) - 70)             
+  .attr("y", 35) 
+  .attr("text-anchor", "middle")  
+    .attr("fill", "black")
+  .style("font-size", "28px")
+  .text("Tree Maintenance Case Status in San Francisco");
+
+  //caption text
+svg.append("text")
+  .attr("x", (1440 / 2) - 50)             
+  .attr("y", 885)
+  .attr("text-anchor", "middle")  
+    .attr("fill", "grey")
+  .style("font-size", "12px")
+  .text("This geospatial map shows the status of tree maitenance in San Francisco. Data from sfgov.org. By Denali Marsh.");
+
+// we want both basemap and streets to load before trees
+// https://github.com/d3/d3-queue
+var q = d3.queue()
+  .defer(d3.json, urls.basemap)
+  .defer(d3.json, urls.streets)
+  .await(drawMap);
+
+function drawMap(error, basemap, streets) {
+  if (error) throw error;
+  console.log("basemap", basemap);
+  console.log("streets", streets);
+
+  // make sure basemap fits in projection
+  projection.fitSize([1340, 850], basemap);
+
+  // draw basemap
+  var land = g.basemap.selectAll("path.land")
+    .data(basemap.features)
+    .enter()
+    .append("path")
+    .attr("d", path)
+    .attr("class", "land")
+
+  // used to show neighborhood outlines on top of streets
+  g.basemap.selectAll("path.neighborhood")
+    .data(basemap.features)
+    .enter()
+    .append("path")
+    .attr("d", path)
+    .attr("class", "neighborhood")
+    .each(function(d) {
+      // save selection in data for interactivity
+      d.properties.outline = this;
+    });
+
+      g.basemap.selectAll("path.street")
+    .data(streets.features)
+    .enter()
+    .append("path")
+    .attr("d", path)
+    .attr("class", "street");
+
+
+  // setup tooltip (shows neighborhood name)
+  var tip = g.tooltip.append("text").attr("id", "tooltip");
+  tip.attr("text-anchor", "end");
+  tip.attr("dx", -5);
+  tip.attr("dy", -5);
+  tip.style("visibility", "hidden");
+
+  // add interactivity
+  land.on("mouseover", function(d) {
+      tip.text(d.properties.neighborho);
+      tip.style("visibility", "visible");
+
+      d3.select(d.properties.outline).raise();
+      d3.select(d.properties.outline).classed("active", true);
+    })
+    .on("mousemove", function(d) {
+      var coords = d3.mouse(g.basemap.node());
+      tip.attr("x", coords[0]);
+      tip.attr("y", coords[1]);
+    })
+    .on("mouseout", function(d) {
+      tip.style("visibility", "hidden");
+      d3.select(d.properties.outline).classed("active", false);
+    });
+
+  d3.json(urls.trees, drawTrees);
+}
+  
+
+
+function drawTrees(error, trees) {
+  if (error) throw error;
+  console.log("trees", trees);
+
+  var symbols = g.trees.selectAll("circle")
+    .data(trees)
+    .enter()
+    .append("circle")
+    .attr("cx", function(d) { return projection([+d.point.longitude, +d.point.latitude])[0]; })
+    .attr("cy", function(d) { return projection([+d.point.longitude, +d.point.latitude])[1]; })
+    .attr("r", 3)
+    .attr("fill", function(d) {return (d.status == "Closed")?"red":"green"});
+
+  // add details widget
+  // https://bl.ocks.org/mbostock/1424037
+  var details = g.details.append("foreignObject")
+    .attr("id", "details")
+    .attr("width", 960)
+    .attr("height", 600)
+    .attr("x", 30)
+    .attr("y", 120);
+
+  var body = details.append("xhtml:body")
+    .style("text-align", "left")
+    .style("background", "none")
+    .html("<p>N/A</p>");
+
+  details.style("visibility", "hidden");
+
+  symbols.on("mouseover", function(d) {
+    d3.select(this).raise();
+    d3.select(this).classed("active", true);
+
+    body.html("<h2> Additonal Information: </h2><table border=0 cellspacing=0 cellpadding=2>" + "\n" +
+      "<tr><th>Case ID:</th><td>" + d.case_id + "</td></tr>" + "\n" +
+      "<tr><th>Category:</th><td>" + d.category + "</td></tr>" + "\n" +
+      "<tr><th>Address:</th><td>" + d.address + "</td></tr>" + "\n" +
+      "<tr><th>Neighborhood:</th><td>" + d.neighborhood + "</td></tr>" + "\n" +
+      "<tr><th>Opened:</th><td>" + d.opened + "</td></tr>" + "\n" +
+      "<tr><th>Status:</th><td>" + d.status + "</td></tr>" + "\n" +
+      "</table>");
+
+    details.style("visibility", "visible");
+  });
+
+  symbols.on("mouseout", function(d) {
+    d3.select(this).classed("active", false);
+    details.style("visibility", "hidden");
+  });
+}
+
+function translate(x, y) {
+  return "translate(" + String(x) + "," + String(y) + ")";
+}
